d3c152eacca7a541997ca20c218f46aadb478da2
mads94i5 <mads94i5@stud.kea.dk>
2022-11-21 13:51:16 +0100

    first

---
+++ src/main/java/carrental/carrentalweb/CarRentalWebApplication.java
@@ -0,0 +1,13 @@
+package carrental.carrentalweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/carrental/carrentalweb/CarRentalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package carrental.carrentalweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
