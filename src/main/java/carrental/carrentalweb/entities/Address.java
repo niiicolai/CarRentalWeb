69aa807edf85e8317fe4d561e89d7f4999ba3e51
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-11-22 14:19:11 +0100

    PickupPoint, PickupPoint og Address created

---
+++ src/main/java/carrental/carrentalweb/entities/Address.java
@@ -0,0 +1,78 @@
+package carrental.carrentalweb.entities;
+
+import java.time.LocalDateTime;
+
+public class Address {
+
+  private String street;
+  private String city;
+  private String zipCode;
+  private String country;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+
+  public Address(String street, String city, String zipCode, String country, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    this.street = street;
+    this.city = city;
+    this.zipCode = zipCode;
+    this.country = country;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  public Address(String street, String city, String zipCode, String country, LocalDateTime createdAt) {
+    this.street = street;
+    this.city = city;
+    this.zipCode = zipCode;
+    this.country = country;
+    this.createdAt = createdAt;
+  }
+
+  public String getStreet() {
+    return street;
+  }
+
+  public void setStreet(String street) {
+    this.street = street;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getZipCode() {
+    return zipCode;
+  }
+
+  public void setZipCode(String zipCode) {
+    this.zipCode = zipCode;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public LocalDateTime getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(LocalDateTime updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+}
---
+++ src/main/java/carrental/carrentalweb/repository/PickupPointRepository.java
@@ -0,0 +1,133 @@
+package carrental.carrentalweb.repository;
+
+import carrental.carrentalweb.entities.Address;
+import carrental.carrentalweb.entities.PickupPoint;
+import carrental.carrentalweb.utilities.MySQLConnector;
+import org.springframework.beans.factory.annotation.Value;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PickupPointRepository {
+  @Value("${JDBCUrl}")
+  private String url;
+  @Value("${JDBCUsername")
+  private String username;
+  @Value("${JDBCPassword")
+  private String password;
+
+  private Connection conn = MySQLConnector.getInstance().getConnection(url, username, password);
+
+  public void createPickupPoint(PickupPoint newPickupPoint) {
+
+    //Connection conn = MySQLConnector.getInstance().getConnection(url, username, password);
+
+    try {
+      String query = "INSERT INTO pickup_points (name," +
+          "address," +
+          "created_at," +
+          "updated_at)" +
+          "VALUES (?, ?, ?)";
+      PreparedStatement preparedStatement = conn.prepareStatement(query);
+
+      preparedStatement.setString(1, newPickupPoint.getName());
+      preparedStatement.setObject(2, newPickupPoint.getAddress());
+      preparedStatement.setObject(3, newPickupPoint.getCreatedAt());
+
+      preparedStatement.executeUpdate();
+
+    } catch (SQLException e){
+      e.printStackTrace();
+    }
+  }
+
+
+  public List<PickupPoint> getPickupPointsList() {
+
+    //Connection conn = MySQLConnector.getInstance().getConnection(url, username, password);
+
+    List<PickupPoint> pickupPoints = new ArrayList<>();
+    try {
+      String query = "SELECT * FROM pickup_points";
+      PreparedStatement preparedStatement = conn.prepareStatement(query);
+      ResultSet resultSet = preparedStatement.executeQuery();
+
+      while (resultSet.next()) {
+        String name = resultSet.getString(1);
+        Address address = (Address) resultSet.getObject(2);
+        LocalDateTime createdAt =  (LocalDateTime) resultSet.getObject(3);
+        LocalDateTime updatedAt =  (LocalDateTime) resultSet.getObject(4);
+
+        pickupPoints.add(new PickupPoint(name,
+            address,
+            createdAt,
+            updatedAt));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return pickupPoints;
+  }
+
+
+  public PickupPoint findPickupPointByName(String findName){
+
+    PickupPoint pickupPoint = new PickupPoint();
+
+    //Connection conn = MySQLConnector.getInstance().getConnection(url, username, password);
+
+    try {
+      String query = "SELECT * FROM pickup_points WHERE name=?";
+      PreparedStatement preparedStatement = conn.prepareStatement(query);
+      preparedStatement.setString(1, findName);
+
+      ResultSet resultSet = preparedStatement.executeQuery();
+
+      while (resultSet.next()){
+        String name = resultSet.getString(1);
+        Address address = (Address) resultSet.getObject(2);
+        LocalDateTime createdAt =  (LocalDateTime) resultSet.getObject(3);
+        LocalDateTime updatedAt =  (LocalDateTime) resultSet.getObject(4);
+
+       pickupPoint = new PickupPoint(name,
+            address,
+            createdAt,
+            updatedAt);
+      }
+    } catch(SQLException e){
+      e.printStackTrace();
+    }
+    return pickupPoint;
+  }
+
+  public void updatePickupPoint (PickupPoint pickupPoint){
+
+    //Connection conn = MySQLConnector.getInstance().getConnection(url, username, password);
+
+    try {
+      String query = "UPDATE pickup_points (name, " +
+          "address, " +
+          "created_at, " +
+          "updated_at)" +
+          "VALUES (?, ?, ?, ?)";
+
+      PreparedStatement preparedStatement = conn.prepareStatement(query);
+
+      preparedStatement.setString(1, pickupPoint.getName());
+      preparedStatement.setObject(2, pickupPoint.getAddress());
+      preparedStatement.setObject(3, pickupPoint.getCreatedAt());
+      preparedStatement.setObject(4, pickupPoint.getUpdatedAt());
+
+      preparedStatement.executeUpdate();
+    } catch (SQLException e){
+      e.printStackTrace();
+    }
+  }
+}
