e20c456c50fa68ee361171957322d61239d11d85
Nicolai Andersen <niiicolai@gmail.com>
2022-11-24 14:59:46 +0100

    add Invoice PDF generator

--- src/main/java/carrental/carrentalweb/controller/HomeController.java
+++ src/main/java/carrental/carrentalweb/controller/HomeController.java
@@ -1,10 +1,18 @@
 package carrental.carrentalweb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import carrental.carrentalweb.entities.Invoice;
+import carrental.carrentalweb.entities.InvoiceSpecification;
+import carrental.carrentalweb.services.InvoicePDFService;
+
 @Controller
 public class HomeController {
+
+    @Autowired
+    InvoicePDFService invoicePDFService;
     
     @GetMapping("/")
     public String index() {
