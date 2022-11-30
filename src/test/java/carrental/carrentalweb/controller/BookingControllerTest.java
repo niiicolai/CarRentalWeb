2b7d2897ef74c9644a7949014dc743ed94fe2b4b
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-11-30 14:37:59 +0100

    BookingControllerTest og PickupPointControllerTest oprettet

---
+++ src/test/java/carrental/carrentalweb/controller/BookingControllerTest.java
@@ -0,0 +1,28 @@
+package carrental.carrentalweb.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingControllerTest {
+
+  @Test
+  void index() {
+  }
+
+  @Test
+  void showCreate() {
+  }
+
+  @Test
+  void create() {
+  }
+
+  @Test
+  void updateBooking() {
+  }
+
+  @Test
+  void editBooking() {
+  }
+}
---
+++ src/test/java/carrental/carrentalweb/controller/PickupPointControllerTest.java
@@ -0,0 +1,32 @@
+package carrental.carrentalweb.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PickupPointControllerTest {
+
+  @Test
+  void index() {
+  }
+
+  @Test
+  void newPickupPoint() {
+  }
+
+  @Test
+  void createPickupPoint() {
+  }
+
+  @Test
+  void updatePickupPoint() {
+  }
+
+  @Test
+  void editPickupPoint() {
+  }
+
+  @Test
+  void deletePickupPoint() {
+  }
+}
