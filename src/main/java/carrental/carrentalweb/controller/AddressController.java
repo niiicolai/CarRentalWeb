9d388fd04a9d5127548bcfeee851df5db5c22d3e
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-12-05 10:18:33 +0100

    Html booking og pickup

--- src/main/java/carrental/carrentalweb/controller/AddressController.java
+++ src/main/java/carrental/carrentalweb/controller/AddressController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AddressController {
@@ -19,10 +20,20 @@ public class AddressController {
   @GetMapping("/address")
   public String index(Model model){
 
-    model.addAttribute("addresslist", ar.getAddressList());
+    model.addAttribute("addressList", ar.getAddressList());
 
     return "address/index";
   }
+
+/*
+  @GetMapping("/pickups/create")
+  public String showChoices(Model model){
+
+    model.addAttribute("addressList", ar.getAddressList());
+
+    return "pickups/create";
+  }
+*/
 
   @GetMapping("/address/create")
   public String showCreate(Model model){
