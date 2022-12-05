2682c60515a976c5aeafcef21333e49a591fa7c2
Nicolai Andersen <niiicolai@gmail.com>
2022-12-05 22:59:30 +0100

    update profile

---
+++ src/main/java/carrental/carrentalweb/services/TimeOfDayService.java
@@ -0,0 +1,54 @@
+package carrental.carrentalweb.services;
+
+import java.util.Calendar;
+
+import org.springframework.stereotype.Service;
+
+import carrental.carrentalweb.enums.TimeOfDay;
+
+@Service
+public class TimeOfDayService {
+    
+    public TimeOfDay get() {
+        Calendar c = Calendar.getInstance();
+        int hourOfDay = c.get(Calendar.HOUR_OF_DAY);
+
+        if (hourOfDay < 12){
+            return TimeOfDay.Morning;       
+        } else if (hourOfDay > 12 && hourOfDay < 16){
+            return TimeOfDay.Afternoon;     
+        } else if (hourOfDay > 16 && hourOfDay < 21){
+            return TimeOfDay.Evening;     
+        } else {
+            return TimeOfDay.Night;     
+        } 
+    }
+
+    public String getGreeting() {
+        TimeOfDay timeOfDay = get();
+        switch(timeOfDay) {
+            case Morning:
+                return "Godmorgen";
+            case Afternoon:
+                return "Godeftermiddag";
+            case Evening:
+                return "Godaften";
+            default:
+                return "Godnat";
+        }
+    }
+
+    public String getImage() {
+        TimeOfDay timeOfDay = get();
+        switch(timeOfDay) {
+            case Morning:
+                return "/images/morning.jpg";
+            case Afternoon:
+                return "/images/afternoon.jpg";
+            case Evening:
+                return "/images/evening.jpg";
+            default:
+                return "/images/night.jpg";
+        }
+    }
+}
