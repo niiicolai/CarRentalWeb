7b65e315cca098f3c38f3b23f72d88609636074d
DizzyMoon <mikkel.simons1@gmail.com>
2022-12-13 11:25:21 +0100

    Added comments

--- src/test/java/carrental/carrentalweb/entity_factories/TestCarFactory.java
+++ src/test/java/carrental/carrentalweb/entity_factories/TestCarFactory.java
@@ -18,7 +18,9 @@ public class TestCarFactory {
      * can be used in Unit and integration tests.
      */
 
-    //Mikkel
+    /*
+    * Written by Mikkel Aabo Simonsen
+     */
     public static Car create(boolean inspected) {
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS");
